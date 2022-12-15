1eb2b7e974009bd04841de41f60948f895874ea9
eva yuji <222190012@student.unaki.ac.id>
2022-12-16 00:30:30 +0700

    commit kelima

--- src/Task.js
+++ src/Task.js
@@ -1,4 +1,4 @@
-const url = "YOURDEPLOYURLhttps://gallery-app-server.vercel.app/photos" // TODO: replace this
+const url = "https://rad-otter-5de71b.netlify.app/photos" // TODO: replace this
 const studentName = "Eva Yuji Anesfia" // TODO: replace this
 const studentId = "FE2710920" // TODO: replace this
 
