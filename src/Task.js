502e81d7d2057b09ab5c8b5bf8f4a321c19581a2
eva yuji <222190012@student.unaki.ac.id>
2022-12-15 18:42:01 +0700

    commit file

---
+++ src/Task.js
@@ -0,0 +1,5 @@
+const url = "YOURDEPLOYURLhttps://gallery-app-server.vercel.app/photos" // TODO: replace this
+const studentName = "Eva Yuji Anesfia" // TODO: replace this
+const studentId = "FE2710920" // TODO: replace this
+
+export { url, studentName, studentId }
